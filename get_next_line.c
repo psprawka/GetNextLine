0eacfc007057da4ac5dc253141bad548ddfc40ff
spraweczga <spraweczga@o2.pl>
2017-10-23 19:35:50 -0700

    get next line changed

--- get_next_line.c
+++ get_next_line.c
@@ -5,87 +5,136 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: psprawka <psprawka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/09/28 12:36:32 by psprawka          #+#    #+#             */
-/*   Updated: 2017/09/28 12:36:34 by psprawka         ###   ########.fr       */
+/*   Created: 2017/10/10 12:41:27 by psprawka          #+#    #+#             */
+/*   Updated: 2017/10/10 12:41:29 by psprawka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "libft/libft.h"
 #include <stdio.h>
 
-static char		*save_the_rest(int *i, char *rest)
+static char		*ft_strjoin_newline(char const *s1, char const *s2)
 {
-	rest = ft_strsub(rest, *i + 1, ft_strlen(rest) - *i - 1);
-	return (rest);
-}
-
-static int		ft_find_line(char **line, char *temp, int fd, int *i)
-{
-	int		characters;
+	char	*f;
+	int		i;
 	
-	while ((characters = read(fd, temp, BUFF_SIZE)) > 0)
+	if (!s1 || !s2)
+		return (NULL);
+	i = 0;
+	if (!(f = (char *)malloc(ft_strlen((char *)s1)
+							 + ft_strlen_chr((char *)s2, '\n') + 1)))
+		return (NULL);
+	while (s1[i])
 	{
-		temp[BUFF_SIZE + 1] == '\0';
-		while (temp[*i] != '\0' && temp[*i] != '\n')
-			*i += 1;
-		
-		*line = ft_strjoin_newline(*line, temp);
-		printf("HERE \n");
-		
-		if (temp[*i] == '\0' && (*i != (BUFF_SIZE + 1)))
-				return (0);
-		if (temp[*i] == '\n')
-			return (1);
-		*i = 0;
+		f[i] = s1[i];
+		i++;
 	}
-	return (0);
+	while (*s2 && *s2 != '\n')
+	{
+		f[i] = *s2++;
+		i++;
+	}
+	f[i] = '\0';
+	return (f);
 }
 
-int				get_next_line(const int fd, char **line)
+static char		*copy_new(char *s, int *len)
 {
-	static char	*rest = NULL;
-	char		temp[BUFF_SIZE + 1];
-	char		*chj;
-	int			if_end;
-	int			i;
-	
-	if (fd < 0 || BUFF_SIZE < 1)
-		return (-1);
-	
-	ft_strclr(*line);
+	int		i;
+	char	*new;
 	i = 0;
 	
-	if (rest != NULL)
+	while ((s[*len] != '\n') && (s[*len] != '\0'))
+		*len += 1;
+	new = (char *)malloc(*len + 1);
+	while ((s[i] != '\n') && (s[i] != '\0'))
 	{
-		chj = rest;;
-		while (chj[i])
-		{
-			if (chj[i] == '\n')
-			{
-				rest = ft_strsub(chj, i, ft_strlen(chj) - i - 1);
-				*line = ft_strdup_newline(chj);
-				return (1);
-			}
-			i++;
-		}
-		*line = rest;
+		new[i] = s[i];
+		i++;
 	}
-	
-	if_end = ft_find_line(line, temp, fd, &i);
-	rest = save_the_rest(&i, temp);
-	return (if_end);
+	new[i] = '\0';
+	return (new);
 }
 
-int main()
+
+static int		find_line(char **line, int fd, char *temp, int *i)
 {
-	int			fd;
-	char		*line;
-	
-	fd = open("file.txt", O_RDONLY);
-	
-	while (get_next_line(fd, &line))
-		printf("%s\n", line);
+	int		ret;
+
+	while ((ret = read(fd, temp, BUFF_SIZE)) > 0)
+	{
+		*i = 0;
+		temp[ret] = '\0';
+		*line = (ret < 0 ? '\0': ft_strjoin_newline(*line, temp));
+		while (*i < ret)
+		{
+			if (temp[*i] == '\n')
+				return (1);
+			*i += 1;
+		}
+		
+		ft_strclr(temp);
+//		ft_bzero(temp, BUFF_SIZE + 1);
+	}
+	if (ret < 0)
+		return (-1);
+	if (ret == 0)
+		return (0);
+	return (1);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	static char	*rest;
+	char		temp[BUFF_SIZE + 1];
+	int			i;
+	int			ret;
+
+	i = 0;
+	if (fd < 0 || !line || BUFF_SIZE < 1)
+		return (-1);
+	*line = ft_memalloc(1);
+	if (rest != NULL)
+	{
+        printf("line: %s, rest: %s\n", *line, rest);
+        *line = copy_new(rest, &i);
+        
+            rest = ft_strsub(rest, i + 1, ft_strlen(rest) - i);
+//        printf("AFTER: i = %d, line %s, rest pocztek%skoniec\n", i, *line, rest);
+//        printf("HEEEEREEEEE\n");
+        if (rest[i] != '\0')
+            rest = NULL;
+            return (1);
+	}
+	else
+		rest = ft_memalloc(1);
+	ret = find_line(line, fd, temp, &i);
+	if (ret == -1)
+		return (-1);
+    
+	if (ret == 1)
+		rest = ft_strsub(temp, i + 1, ft_strlen(temp) - i);
+    if (rest[0] == '\0')
+        free(rest);
+	if (*rest == '\0' && **line == '\0')
+		return (0);
+	return (1);
+}
+
+int main(int argc, char **argv)
+{
+	char *line;
+	int fd;
+	int i;
+	int ret = 0;
+
+	if (argc != 2)
+		return (0);
+	fd = open(argv[1], O_RDONLY);
+	i = 0;
+	while ((ret = get_next_line(fd, &line)))
+		printf("RET: [%d] Line: [%s]\n", ret, line);
+	printf("RET: [%d]\n",ret);
 	close(fd);
 	return 0;
 }
--- get_next_line.h
+++ get_next_line.h
@@ -5,32 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: psprawka <psprawka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/09/28 12:36:54 by psprawka          #+#    #+#             */
-/*   Updated: 2017/09/28 12:36:57 by psprawka         ###   ########.fr       */
+/*   Created: 2017/10/10 12:41:16 by psprawka          #+#    #+#             */
+/*   Updated: 2017/10/10 12:41:18 by psprawka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 3
+# define BUFF_SIZE 12
 # include <stdlib.h>
 # include <unistd.h>
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
-
-typedef struct	s_line
-{
-	int				fd;
-	char			*rest;
-	struct s_line	*next;
-	
-}				t_line;
-
+# include "libft/includes/libft.h"
 
 int			get_next_line(const int fd, char **line);
-t_line		*ft_list_find(t_line *begin_list, int fd, int (*cmp)());
-t_line		*ft_listnew(void const *rest, int fd);
 
 #endif
