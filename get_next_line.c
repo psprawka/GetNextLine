000b8f33723333e35d1e5b85500bc8519961c163
spraweczga <spraweczga@o2.pl>
2017-11-01 10:32:02 -0700

    Fixed buff

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,14 @@
 /*   By: psprawka <psprawka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/10 12:41:27 by psprawka          #+#    #+#             */
-/*   Updated: 2017/10/10 12:41:29 by psprawka         ###   ########.fr       */
+/*   Updated: 2017/10/30 21:40:46 by psprawka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-static char		*ft_strjoin_newline(char const *s1, char const *s2)
+static char		*ft_strjoin_newline(char const *s1, char const *s2, int *only_nl)
 {
 	char	*f;
 	int		i;
@@ -21,9 +21,9 @@ static char		*ft_strjoin_newline(char const *s1, char const *s2)
 	if (!s1 || !s2)
 		return (NULL);
 	i = 0;
-	if (!(f = (char *)malloc(ft_strlen((char *)s1)
-							 + ft_strlen_chr((char *)s2, '\n') + 1)))
+	if (!(f = (char *)malloc(ft_strlen((char *)s1) + ft_strlen_chr((char *)s2, '\n') + 1)))
 		return (NULL);
+	*only_nl = ((BUFF_SIZE == 1 && *s2 == '\n') ? 1 : 0);
 	while (s1[i])
 	{
 		f[i] = s1[i];
@@ -43,9 +43,9 @@ static char		*copy_new(char *s, int *len)
 	int		i;
 	char	*new;
 	i = 0;
-	
 	while ((s[*len] != '\n') && (s[*len] != '\0'))
 		*len += 1;
+//	printf("STRING: [%s]\ns[%d] = [%c]\n", s, *len, s[*len]);
 	new = (char *)malloc(*len + 1);
 	while ((s[i] != '\n') && (s[i] != '\0'))
 	{
@@ -60,12 +60,18 @@ static char		*copy_new(char *s, int *len)
 static int		find_line(char **line, int fd, char *temp, int *i)
 {
 	int		ret;
+	int		only_nl;
 
+	only_nl = 0;
 	while ((ret = read(fd, temp, BUFF_SIZE)) > 0)
 	{
 		*i = 0;
 		temp[ret] = '\0';
-		*line = (ret < 0 ? '\0': ft_strjoin_newline(*line, temp));
+		*line = (ret < 0 ? '\0': ft_strjoin_newline(*line, temp, &only_nl));
+//		printf("ONLY_NL = %d\n", only_nl);
+		if (only_nl == 1)
+			return (1);
+//		printf("HERE\n");
 		while (*i < ret)
 		{
 			if (temp[*i] == '\n')
@@ -74,7 +80,6 @@ static int		find_line(char **line, int fd, char *temp, int *i)
 		}
 		
 		ft_strclr(temp);
-//		ft_bzero(temp, BUFF_SIZE + 1);
 	}
 	if (ret < 0)
 		return (-1);
@@ -94,47 +99,47 @@ int		get_next_line(const int fd, char **line)
 	if (fd < 0 || !line || BUFF_SIZE < 1)
 		return (-1);
 	*line = ft_memalloc(1);
-	if (rest != NULL)
-	{
-        printf("line: %s, rest: %s\n", *line, rest);
-        *line = copy_new(rest, &i);
-        
-            rest = ft_strsub(rest, i + 1, ft_strlen(rest) - i);
-//        printf("AFTER: i = %d, line %s, rest pocztek%skoniec\n", i, *line, rest);
-//        printf("HEEEEREEEEE\n");
-        if (rest[i] != '\0')
-            rest = NULL;
-            return (1);
-	}
-	else
+	if (rest == NULL)
 		rest = ft_memalloc(1);
+	
+	if (*rest != '\0')
+	{
+
+        *line = copy_new(rest, &i);
+          if (rest[i] == '\n')
+		  {
+			  rest = ft_strsub(rest, i + 1, ft_strlen(rest) - i);
+			  return (1);
+		  }
+		rest = ft_strsub(rest, i + 1, ft_strlen(rest) - i);
+	}
 	ret = find_line(line, fd, temp, &i);
+	if (ret == 1 && BUFF_SIZE == 1)
+		return (1);
+//	printf("ret = %d\n", ret);
 	if (ret == -1)
 		return (-1);
-    
 	if (ret == 1)
 		rest = ft_strsub(temp, i + 1, ft_strlen(temp) - i);
-    if (rest[0] == '\0')
-        free(rest);
 	if (*rest == '\0' && **line == '\0')
 		return (0);
 	return (1);
 }
 
-int main(int argc, char **argv)
-{
-	char *line;
-	int fd;
-	int i;
-	int ret = 0;
-
-	if (argc != 2)
-		return (0);
-	fd = open(argv[1], O_RDONLY);
-	i = 0;
-	while ((ret = get_next_line(fd, &line)))
-		printf("RET: [%d] Line: [%s]\n", ret, line);
-	printf("RET: [%d]\n",ret);
-	close(fd);
-	return 0;
-}
+//int main(int argc, char **argv)
+//{
+//	char *line;
+//	int fd;
+//	int i;
+//	int ret = 0;
+//
+//	if (argc != 2)
+//		return (0);
+//	fd = open(argv[1], O_RDONLY);
+//	i = 0;
+//	while ((ret = get_next_line(42, &line)) > 0)
+//		printf("RET: [%d] Line: [%s]\n", ret, line);
+//	printf("RET: [%d]\n",ret);
+//	close(fd);
+//	return 0;
+//}
